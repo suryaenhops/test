98ae3d23351c98df607f4dea70d137b1f7466ce4
EIN0043 <EIN0043@ESL008.jstlglobal.com>
2019-06-13 19:55:05 +0530

    time added

--- Practice/src/test/java/Base/TestBase.java
+++ Practice/src/test/java/Base/TestBase.java
@@ -1,5 +1,7 @@
 package Base;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -16,7 +18,9 @@ public class TestBase {
 	public static void startBrowser(){		
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/resources/drivers/chromedriver.exe");
 		driver= new ChromeDriver();
-		driver.get("https://www.creditmantri.com/");		
+		driver.get("https://www.creditmantri.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
 	}
 	
 	
